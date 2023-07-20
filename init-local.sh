726074774c6ab7906045fcf0e8003b219d40898d
chandiniv1 <velamalachandini9@gmail.com>
2023-07-20 10:27:30 +0530

    started gm for avail

--- init-local.sh
+++ init-local.sh
@@ -48,7 +48,7 @@ gmd collect-gentxs
 sed -i '' 's/cors_allowed_origins = \[\]/cors_allowed_origins = ["*"]/g' ~/.gm/config/config.toml
 
 # start the chain
-gmd start --rollkit.aggregator true --rollkit.da_layer celestia --rollkit.da_config='{"base_url":"http://localhost:26659","timeout":60000000000,"fee":6000,"gas_limit":6000000}' --rollkit.namespace_id $NAMESPACE_ID --rollkit.da_start_height $DA_BLOCK_HEIGHT --api.enable --api.enabled-unsafe-cors
-
+#gmd start --rollkit.aggregator true --rollkit.da_layer celestia --rollkit.da_config='{"base_url":"http://localhost:26659","timeout":60000000000,"fee":6000,"gas_limit":6000000}' --rollkit.namespace_id $NAMESPACE_ID --rollkit.da_start_height $DA_BLOCK_HEIGHT --api.enable --api.enabled-unsafe-cors
+gmd start --rollkit.aggregator true --rollkit.da_layer avail --rollkit.da_config='{ "seed":"bottom drive obey lake curtain smoke basket hold race lonely fit walk//Alice","api_url":"ws://127.0.0.1:9944","size":1000,"app_id" : 0, "dest" : "5H3qehpRTFiB3XwyTzYU43SpG7e8jW87vFug95fxdew76Vyi","amount" : 10}' --rollkit.namespace_id $NAMESPACE_ID --rollkit.da_start_height 149448 --api.enable --api.enabled-unsafe-cors
 # uncomment the next command if you are using lazy aggregation
 # gmd start --rollkit.aggregator true --rollkit.da_layer celestia --rollkit.da_config='{"base_url":"http://localhost:26659","timeout":60000000000,"fee":6000,"gas_limit":6000000}' --rollkit.namespace_id $NAMESPACE_ID --rollkit.da_start_height $DA_BLOCK_HEIGHT --rollkit.lazy_aggregator
--- init-testnet.sh
+++ init-testnet.sh
@@ -20,5 +20,5 @@ gmd keys add $KEY_NAME --keyring-backend test
 gmd add-genesis-account $KEY_NAME $TOKEN_AMOUNT --keyring-backend test
 gmd gentx $KEY_NAME $STAKING_AMOUNT --chain-id $CHAIN_ID --keyring-backend test
 gmd collect-gentxs
-gmd start --rollkit.aggregator true --rollkit.da_layer celestia --rollkit.da_config='{"base_url":"http://localhost:26659","timeout":60000000000,"fee":6000,"gas_limit":6000000}' --rollkit.namespace_id $NAMESPACE_ID --rollkit.da_start_height $DA_BLOCK_HEIGHT
-
+#gmd start --rollkit.aggregator true --rollkit.da_layer celestia --rollkit.da_config='{"base_url":"http://localhost:26659","timeout":60000000000,"fee":6000,"gas_limit":6000000}' --rollkit.namespace_id $NAMESPACE_ID --rollkit.da_start_height $DA_BLOCK_HEIGHT
+gmd start --rollkit.aggregator true --rollkit.da_layer avail --rollkit.da_config='{ "seed":"bottom drive obey lake curtain smoke basket hold race lonely fit walk//Alice","api_url":"ws://127.0.0.1:9944","size":1000,"app_id" : 0, "dest" : "5H3qehpRTFiB3XwyTzYU43SpG7e8jW87vFug95fxdew76Vyi","amount" : 10}' --rollkit.namespace_id $NAMESPACE_ID --rollkit.da_start_height 149448 --api.enable --api.enabled-unsafe-cors
